da416d64e219ff75f82b2fd29b5709112a6b43eb
Thomas Bechtold <tbechtold@suse.com>
2017-05-22 11:24:11 +0200

    Do not require oslotest for testing
    oslotest itself depends on debtcollector and debtcollector depends
    on oslotest. That's a cycle dependency which makes bootstrapping the whole
    module chain more difficult.
    Instead of using oslotest, just use the standard unittest.TestCase class
    as base to break the dependency cycle.
    
    Change-Id: Idcbe727883fe2742d62d463bb9a195592aeaba09

--- debtcollector/tests/base.py
+++ debtcollector/tests/base.py
@@ -15,9 +15,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from oslotest import base
+import unittest
 
 
-class TestCase(base.BaseTestCase):
+class TestCase(unittest.TestCase):
 
     """Test case base class for all unit tests."""
